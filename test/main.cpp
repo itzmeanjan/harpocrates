bd40ee82fa659f8ce6db719047ecaf8aec3ddd17
Anjan Roy <hello@itzmeanjan.in>
2022-05-11 11:59:52 +0530

    test implementation conformance with standard using KATs
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- test/main.cpp
+++ test/main.cpp
@@ -1,17 +1,23 @@
 #include "test_harpocrates.hpp"
+#include <bit>
 #include <iostream>
 #include <string.h>
 
 int
 main()
 {
+  test_harpocrates_kat();
+  std::cout << "[test] Harpocrates known answer tests ( KATs ) passed !"
+            << std::endl;
+
   constexpr size_t itr_cnt = 1ul << 10;
 
   for (size_t i = 0; i < itr_cnt; i++) {
     test_harpocrates();
   }
 
-  std::cout << "[test] Harpocrates cipher works !" << std::endl;
+  std::cout << "[test] Harpocrates random encrypt -> decrypt works !"
+            << std::endl;
 
   return EXIT_SUCCESS;
 }
