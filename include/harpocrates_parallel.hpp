d3e579ae1be15d8d64aeb47e90e338c2f0184a7f
Anjan Roy <hello@itzmeanjan.in>
2022-05-13 12:13:24 +0530

    data parallel harpocrates cipher, using SYCL
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

---
+++ include/harpocrates_parallel.hpp
@@ -0,0 +1,133 @@
+#pragma once
+#include "harpocrates.hpp"
+#include <CL/sycl.hpp>
+#include <cassert>
+
+// Data Parallel Harpocrates - An Efficient Parallel Encryption Mechanism for
+// Data-at-rest, offloading to CPU & GPU using SYCL
+//
+// Read more about SYCL here
+// https://www.khronos.org/registry/SYCL/specs/sycl-2020/html/sycl-2020.html
+namespace harpocrates_parallel {
+
+class kernelHarpocratesEncrypt;
+class kernelHarpocratesDecrypt;
+
+sycl::event
+encrypt(sycl::queue,
+        const uint8_t* const __restrict,
+        const uint8_t* const __restrict,
+        uint8_t* const __restrict,
+        const size_t,
+        const size_t,
+        std::vector<sycl::event>);
+
+sycl::event
+decrypt(sycl::queue,
+        const uint8_t* const __restrict,
+        const uint8_t* const __restrict,
+        uint8_t* const __restrict,
+        const size_t,
+        const size_t,
+        std::vector<sycl::event>);
+
+// Given 256 -bytes look up table & N -bytes plain text, this routine offloads
+// computation of encrypted byte slices to available accelerator device (
+// encapsulated in SYCL queue object ), producing N -many encrypted bytes.
+//
+// For encrypting each 16 -bytes slice, this routine makes use of Harpocrates
+// cipher. Which is exactly why N must be evenly divisible by 16.
+//
+// Also note, dispatched work group size must be evenly dividing `ct_len >> 4`,
+// so that each work-group has same number of active work-items
+//
+// For creating SYCL dependency graph, make use of last parameter & return type
+// of this routine.
+sycl::event
+encrypt(sycl::queue q,                       // SYCL queue
+        const uint8_t* const __restrict lut, // 256 -bytes look up table
+        const uint8_t* const __restrict txt, // plain text | len(txt) = N
+        uint8_t* const __restrict enc,       // encrypted text | len(enc) = N
+        const size_t ct_len,                 // len(txt) = len(enc) = N
+        const size_t wg_size,                // (ct_len >> 4) % wg_size == 0
+        std::vector<sycl::event> evts        // SYCL events to wait for
+)
+{
+  // ensure input plain text is of non-zero length
+  assert(ct_len > 0ul);
+  // ensure input plain text length is evenly divisible by 16,
+  // so that we can dispatch `ct_len >> 4` -many work-items
+  assert((ct_len & 15ul) == 0ul);
+
+  // these many work-items to be dispatched so that each work-item
+  // can encrypt its portion of plain text independently
+  const size_t wi_cnt = ct_len >> 4;
+
+  // all work-groups must have same #- of active work-items
+  assert(wi_cnt % wg_size == 0);
+
+  return q.submit([&](sycl::handler& h) {
+    // create dependency graph
+    h.depends_on(evts);
+
+    const auto rng = sycl::nd_range<1>{ wi_cnt, wg_size };
+    h.parallel_for<kernelHarpocratesEncrypt>(rng, [=](sycl::nd_item<1> it) {
+      const size_t idx = it.get_global_linear_id();
+      const size_t b_off = idx << 4;
+
+      harpocrates::encrypt(lut, txt + b_off, enc + b_off);
+    });
+  });
+}
+
+// Given 256 -bytes inverse look up table & N -bytes cipher text, this routine
+// offloads computation of decrypted byte slices to available accelerator device
+// ( encapsulated in SYCL queue object ), producing N -many decrypted bytes.
+//
+// For decrypting each 16 -bytes slice, this routine makes use of Harpocrates
+// cipher. Which is exactly why N must be evenly divisible by 16.
+//
+// Also note, dispatched work group size must be evenly dividing `ct_len >> 4`,
+// so that each work-group has same number of active work-items
+//
+// For creating SYCL dependency graph, make use of last parameter & return type
+// of this routine.
+sycl::event
+decrypt(
+  sycl::queue q,                           // SYCL queue
+  const uint8_t* const __restrict inv_lut, // 256 -bytes inverse look up table
+  const uint8_t* const __restrict enc,     // encrypted bytes | len(enc) = N
+  uint8_t* const __restrict dec,           // decrypted bytes | len(dec) = N
+  const size_t ct_len,                     // len(enc) = len(dec) = N
+  const size_t wg_size,                    // (ct_len >> 4) % wg_size == 0
+  std::vector<sycl::event> evts            // SYCL events to wait for
+)
+{
+  // ensure input cipher text is of non-zero length
+  assert(ct_len > 0ul);
+  // ensure input cipher text length is evenly divisible by 16,
+  // so that we can dispatch `ct_len >> 4` -many work-items
+  assert((ct_len & 15ul) == 0ul);
+
+  // these many work-items to be dispatched so that each work-item
+  // can decrypt its portion of cipher text independently
+  const size_t wi_cnt = ct_len >> 4;
+
+  // all work-groups must have same #- of active work-items
+  assert(wi_cnt % wg_size == 0);
+
+  return q.submit([&](sycl::handler& h) {
+    // create dependency graph
+    h.depends_on(evts);
+
+    const auto rng = sycl::nd_range<1>{ wi_cnt, wg_size };
+    h.parallel_for<kernelHarpocratesDecrypt>(rng, [=](sycl::nd_item<1> it) {
+      const size_t idx = it.get_global_linear_id();
+      const size_t b_off = idx << 4;
+
+      harpocrates::decrypt(inv_lut, enc + b_off, dec + b_off);
+    });
+  });
+}
+
+}
