3d6d030aa43e68a589610737e0f16b6acd1ea999
Anjan Roy <hello@itzmeanjan.in>
2022-05-16 10:51:22 +0530

    test harpocrates wrapper API
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- wrapper/rust/src/harpocrates.rs
+++ wrapper/rust/src/harpocrates.rs
@@ -49,3 +49,41 @@ pub fn decrypt(ilut: &[u8], enc: &[u8], dec: &mut [u8]) {
         harpocrates_decrypt(ilut.as_ptr(), enc.as_ptr(), dec.as_mut_ptr());
     }
 }
+
+mod test {
+    #[test]
+    fn harpocrates_cipher() {
+        use crate::harpocrates::*;
+        use rand::prelude::*;
+
+        let mut lut = Vec::<u8>::with_capacity(256);
+        let mut ilut = Vec::<u8>::with_capacity(256);
+        let mut txt = Vec::<u8>::with_capacity(16);
+        let mut enc = Vec::<u8>::with_capacity(16);
+        let mut dec = Vec::<u8>::with_capacity(16);
+
+        unsafe {
+            lut.set_len(256);
+            ilut.set_len(256);
+            txt.set_len(16);
+            enc.set_len(16);
+            dec.set_len(16);
+        }
+
+        generate_lut(&mut lut);
+        generate_ilut(&lut, &mut ilut);
+
+        let mut rng = thread_rng();
+
+        rng.fill_bytes(&mut txt);
+        enc.fill(0u8);
+        dec.fill(0u8);
+
+        encrypt(&lut, &txt, &mut enc);
+        decrypt(&ilut, &enc, &mut dec);
+
+        for i in 0..16 {
+            assert_eq!(txt[i], dec[i]);
+        }
+    }
+}
